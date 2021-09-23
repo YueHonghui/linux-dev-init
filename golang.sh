1188ee8097346c4505e9382b8b2ddcd0d2926b1d
YueHonghui <hongf.yue@hotmail.com>
2021-09-23 23:34:51 +0800

    upgrade golang

--- golang.sh
+++ golang.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# install golang 1.16.6
+# install golang 1.17.1
 mkdir -p ~/.local/gotmp
 mkdir -p ~/.local/gobin
 mkdir -p ~/.local/bin
 mkdir -p ~/.gomodcache
 cd ~/.local/gotmp && \
-  wget "https://golang.org/dl/go1.16.6.linux-amd64.tar.gz" &&\
-  tar -zxvf go1.16.6.linux-amd64.tar.gz && \
-  mv go ../go1.16.6 && \
-  cd - && ln -s ~/.local/go1.16.6 ~/.local/go
+  wget "https://golang.org/dl/go1.17.1.linux-amd64.tar.gz" &&\
+  tar -zxvf go1.17.1.linux-amd64.tar.gz && \
+  mv go ../go1.17.1 && \
+  cd - && ln -s ~/.local/go1.17.1 ~/.local/go
 echo "GOROOT=${HOME}/.local/go" >> ~/.go.env
 echo "GOPROXY=https://goproxy.cn,direct" >> ~/.go.env
 echo "GOMODCACHE=${HOME}/.gomodcache" >> ~/.go.env
